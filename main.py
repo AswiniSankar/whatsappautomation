5aa9df77820a400fa13beabce51b13cf3682e2c5
Aswini Sankar <53120389+AswiniSankar@users.noreply.github.com>
2021-06-21 10:05:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import pywhatkit as kt
+kt.sendwhatmsg("+91**********", "How are you",10,00)
+#1st parameter = senders phone number with country code
+#2nd parameter = what the message that you want to send
+#3rd parameter = time which is a 24 hour format
+#4th parameter = minute
